add3369c3fd15b36c8306e9cb8a9d11d415a25e1
saicharanbandi <saicharanbandi1208@gmail.com>
2023-08-08 02:06:47 +0530

    added work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,41 +1,68 @@
-import React from 'react'
-import reactshopsense from '../assets/projects/reactshopsense.png'
-import namastefoodapp from '../assets/projects/namastefoodapp.png'
-import weather from "../assets/projects/weather.png"
-import todo from '../assets/projects/todo.png'
+import React from 'react';
+import { data } from "../data/data.js";
+
 
 const Work = () => {
-  return (
-    <div className='bg-[#0a192f] w-full md:h-screen text-gray-300' name="work">
-        {/* work container */}
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl border-b-4 border-pink-600 inline font-bold'>Work</p>
-                <p className='py-6'>Check out some of my recent work</p>
-            </div>
-            {/* below is the card item div */}
-            <div style={{ backgroundImage: `url(${reactshopsense})`}} className='grid sm:grid-cols-2 md:grid-cols-3 gap-4' >
-                {/* card item  */}
-                <div className='shadow-lg shadow-[#040c16] group container rounded-sm flex justify-center items-center mx-auto content-div'>
-                    {/* hover effects for cards */}
-                    <div>
-                        <span>
-                            {/* <img src={reactshopsense}/> */}
-                        </span>
-                        <div>
-                            <a href='/'>
-                                <button>Demo</button>
-                            </a>
-                            <a href='/'>
-                                <button>GitHub</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-}
 
-export default Work
+    // projects file
+    const project = data;
+    //setProject(data);
+  
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+            Work
+          </p>
+          <p className='py-6'>// Check out some of my recent work</p>
+        </div>
+
+{/* container for projects */}
+<div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          
+          {/* Gird Item */}
+          {project.map((item, index) => (
+  <div
+    key={index}
+    style={{ backgroundImage: `url(${item.image})` }}
+    className="shadow-lg shadow-[#040c16] group container rounded-md 
+              flex justify-center text-center items-center mx-auto content-div "
+  >
+    {/* Hover effect for images */}
+    <div className="opacity-0 group-hover:opacity-100 ">
+      <span className="text-2xl font bold text-white tracking-wider ">
+        {item.name}
+      </span>
+      <div className="pt-8 text-center ">
+        {/* eslint-disable-next-line */}
+        <a href={item.github} target="_blank">
+          <button
+            className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+          >
+            Code
+          </button>
+        </a>
+        {/* eslint-disable-next-line */}
+        <a href={item.live} target="_blank">
+          <button
+            className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+          >
+            Live
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+))}
+
+
+</div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import reactshopsense from "../assets/projects/reactshopsense.png";
+import namastefoodapp from "../assets/projects/namastefoodapp.png";
+import weather from "../assets/projects/weather.png";
+import todo from "../assets/projects/todo.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "React JS Application",
+    image: reactshopsense,
+    github: "https://github.com/Charan2409/ReactShopSense",
+    live: "https://react-shop-sense.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "React JS Application",
+    image: namastefoodapp,
+    github: "https://github.com/Charan2409/Namaste_FoodApp",
+    live: "not deployed yet",
+  },
+  {
+    id: 3,
+    name: "React JS Application",
+    image: weather,
+    github: "https://github.com/Charan2409/Weather",
+    live: "https://weather-beryl-nu.vercel.app/",
+  },
+  {
+    id: 4,
+    name: "React JS Application",
+    image: todo,
+    github: "https://github.com/Charan2409/Todolist-react",
+    live: "https://todolist-react-jet.vercel.app/",
+  },
+];
