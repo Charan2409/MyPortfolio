68f87e66dd0ea154fbc60c3b110f1c174e54664e
saicharanbandi <saicharanbandi1208@gmail.com>
2023-08-08 18:57:42 +0530

    fixed the build issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import SC from '../assets/SC.png'
-import {FaBars,FaTimes,FaGithub, FaLinkedin} from 'react-icons/Fa'
+import {FaBars,FaTimes,FaGithub, FaLinkedin} from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import { Link } from 'react-scroll';
